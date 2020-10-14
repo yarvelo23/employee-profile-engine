469cae294b6a9efee3145b9976ea7133830e3d64
Yerson Arvelo <yerson.a06@gmail.com>
2020-10-13 21:12:02 -0400

    manager creation function

--- Develop/app.js
+++ Develop/app.js
@@ -10,17 +10,45 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const questions = [
+// creating empty team member array to take input
+const teamMembers = [];
+const emptyId = [];
+
+const employeePrompt = [
     {
         type: "input",
-        name: "name",
-        message: "What is your name?"
+        name: "nameManager",
+        message: "What is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's ID?"
+    },
+    {
+        type: "input",
+        name: "emailManager",
+        message: "What is the manager's email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?"
     }
 ];
 
-inquirer.prompt(questions).then(function(data){
-    console.log(data.name);
-})
+function manager() {
+    console.log("Assembling your team!");
+    inquirer.prompt(employeePrompt).then(function(data){
+        console.log(data.nameManager);
+        const manager = new Manager(data.nameManager, data.managerId, data.emailManager, data.officeNumber);
+        teamMembers.push(manager);
+        emptyId.push(data.managerId);
+        console.log(teamMembers);
+        console.log(emptyId);
+        team();
+    });
+};
 
 
 

a8c848f672954a6989f8aa4d5a3dedd99862af3f
Yerson Arvelo <yerson.a06@gmail.com>
2020-10-13 21:26:18 -0400

    function to create engineer profile

--- Develop/app.js
+++ Develop/app.js
@@ -68,9 +68,37 @@ function team() {
             engineer();
         } else if (data.memberChoice === "Intern"){
             console.log("Intern");
-            Intern();
+            intern();
         } else (outputTeam());
     });
 };
 
-
+function engineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's ID?"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email?"
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's GitHub username?"
+        }
+    ]).then(function(data){
+        const engineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGithub);
+        teamMembers.push(engineer);
+        emptyId.push(data.engineerId);
+        team();
+    });
+};
